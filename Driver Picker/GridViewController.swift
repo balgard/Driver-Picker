ef6655d4560d8fcb3d2b774107f9184bc11a88bf
Victoria Anderson <VictoriaAnderson@bluc02mg2cdfh51.local>
2018-01-22 12:35:07 -0600

    grid view

--- Driver Picker/GridViewController.swift
+++ Driver Picker/GridViewController.swift
@@ -13,7 +13,22 @@ class GridViewController:UIViewController
     //Empty array of names to be filled on segue through realm
     
     var students = [String]()
-
+     @IBOutlet weak var myLabel: UILabel!
+     let reuseIdentifier = "cell"
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
+    {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! GridViewController
+        cell.myLabel.text = self.students[indexPath.item]
+    }
+    
+    
     
     
     override func viewDidLoad()
